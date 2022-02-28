5ed474369f308056baee0b56dfed7e1f5e2df855
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2022-02-28 09:57:15 -0800

    adds navbar component

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,15 +1,20 @@
 import './App.css';
 import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Auth from '../AuthPage/Auth'
 import NewOrder from '../NewOrderPage/NewOrder'
 import OrderHistory from '../OrderHistoryPage/OrderHistory'
 
 export default function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState({});
   return (
     <main className="App">
       { user ?
-          <NewOrder/>
+          <Routes>
+            {/* route components in here */}
+            <Route path="/orders/new" element={<NewOrder/>} />
+            <Route path="/orders" element={<OrderHistory/>}/>
+          </Routes>
           :
           <Auth/>
 
