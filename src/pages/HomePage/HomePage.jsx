0a3daf1ca39a403d7e4ea75327e6be4cb37225e2
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2022-07-19 16:56:14 -0700

    styles home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -15,37 +15,55 @@ export default function HomePage() {
                 </div>
             </section>
             <section className="home-order-container">
-                <div className="home-gradient-left"></div>
+                <div id="left-order-container" className="home-order-image-container">
+                    <div className="home-order-image-subcontainer">
+                        <h1 className="home-order-image-titles">Natural</h1>
+                        <img className="home-order-images" src="https://imgur.com/zCJ3siV.png" alt="" />
+                    </div>
+                    <div className="home-order-image-subcontainer">
+                        <h1 className="home-order-image-titles">Fruity</h1>
+                        <img className="home-order-images" src="https://imgur.com/lVLFgX0.png" alt="" />
+                    </div>
+                </div>
                 <div className="home-order-body-container">
-                    <h1 className="home-order-body">START EXPLORING</h1>
+                    <h1 className="home-order-body-title">START EXPLORING</h1>
                     <p className="home-order-body">We have a wide variety of different scents, and check out our seasonals
                         before they go
                     </p>
                     <button className="home-order-body" id="home-order-button">ORDER NOW</button>
                 </div>
-                <div className="home-gradient-right"></div>
+                <div id="right-order-container" className="home-order-image-container">
+                    <div className="home-order-image-subcontainer">
+                        <h1 className="home-order-image-titles">Floral</h1>
+                        <img className="home-order-images" src="https://imgur.com/uXs82zm.png" alt="" />
+                    </div>
+                    <div className="home-order-image-subcontainer">
+                        <h1 className="home-order-image-titles">Masculine</h1>
+                        <img className="home-order-images" src="https://imgur.com/6Z4SmwJ.png" alt="" />
+                    </div>
+                </div>
             </section>
             <h1 className="home-feature-title">FEATURED</h1>
             <section className="home-feature-container">
                     <Link id="link-1" className="home-feature-links" to="/soaps">
-                        <h1 className="home-feature-image-titles">TITLEs</h1>
-                        <img className="home-feature-images" src="https://imgur.com/0X6IChc.png" alt="" />
+                        <h1 id="home-feature-image-titles-lrg" className="home-feature-image-titles">Black Jasmine</h1>
+                        <img className="home-feature-images" src="https://imgur.com/GmIZZey.png" alt="" />
                     </Link>
                     <Link id="link-2" className="home-feature-links" to="/soaps">
-                        <h1 className="home-feature-image-titles">TITLEs</h1>
-                        <img className="home-feature-images" src="https://imgur.com/0X6IChc.png" alt="" />
+                        <h1 className="home-feature-image-titles">Sea Salt Agave</h1>
+                        <img className="home-feature-images" src="https://imgur.com/Rcmqln0.png" alt="" />
                     </Link>
                     <Link id="link-3" className="home-feature-links" to="/soaps">
-                        <h1 className="home-feature-image-titles">TITLEs</h1>
-                        <img className="home-feature-images" src="https://imgur.com/0X6IChc.png" alt="" />
+                        <h1 className="home-feature-image-titles">Rejuvination</h1>
+                        <img className="home-feature-images" src="https://imgur.com/4jCRkug.png" alt="" />
                     </Link>
                     <Link id="link-4" className="home-feature-links" to="/soaps">
-                        <h1 className="home-feature-image-titles">TITLEs</h1>
-                        <img className="home-feature-images" src="https://imgur.com/bmQHIeI.png" alt="" />
+                        <h1 className="home-feature-image-titles">Black Rasberry Vanilla</h1>
+                        <img className="home-feature-images" src="https://imgur.com/PtNeTrK.png" alt="" />
                     </Link>
                     <Link id="link-5" className="home-feature-links" to="/soaps">
-                        <h1 className="home-feature-image-titles">TITLEs</h1>
-                        <img className="home-feature-images" src="https://imgur.com/GmIZZey.png" alt="" />   
+                        <h1 className="home-feature-image-titles">Lemon Loaf</h1>
+                        <img className="home-feature-images" src="https://imgur.com/hFmEvN3.png" alt="" />   
                     </Link>
             </section>
         </>
