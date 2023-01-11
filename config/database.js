41156c82b8eabdd0cd49664e14ec64011e18baec
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2023-01-11 11:37:39 -0800

    test

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // mongoose.connect(process.env.DATABASE_URL);
-mongoose.connect(process.env.MONGODB_URI + 'VasquezFarms', {
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
