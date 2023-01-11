62382c92b5bf23244609798dd28be1da61619c18
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2023-01-11 11:33:52 -0800

    I am Amazing

--- config/database.js
+++ config/database.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URL);
-mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect(process.env.MONGODB_URI + 'VasquezFarms', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
 
 db.on('connected', function () {
   console.log(`Connected to ${db.name} at ${db.host}:${db.port}`);
+});
+
+db.on('error', function (err) {
+  console.log(`MongoDb error: ${err}`);
 });
