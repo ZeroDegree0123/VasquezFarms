c5f765cbee9dd2d5126c314f04e083067f5f0c39
ZeroDegree0123 <zachary.vasquez.w@gmail.com>
2023-05-15 10:30:31 -0700

    styles about page, and soapdetails page

--- src/components/ProfileComponent/ProfileNav/ProfileNav.jsx
+++ src/components/ProfileComponent/ProfileNav/ProfileNav.jsx
@@ -5,7 +5,7 @@ export default function profileNav({handleLogOut}) {
     return (
         <> 
             <section className="profile-nav-container">
-                <Link className="profile-nav-links" to="/">Home</Link>
+                <Link className="profile-nav-links" to="/">&#60; Home</Link>
                 &nbsp;
                 <Link className="profile-nav-links" to="/profile/overview">Overview</Link>
                 &nbsp;
